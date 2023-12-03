2f9414ffadcc25fc2ef07b7f264cbf59c146c877
FilipDavid1 <filipenkodavid@gmail.com>
2023-12-03 13:58:23 +0100

    fix infinite map for 1450 screen width

--- src/Mapa.java
+++ src/Mapa.java
@@ -9,21 +9,23 @@ public class Mapa {
     private Obrazok mapaImg;
     private int x;
     private int y;
+    private int mapWidth;
     
     public Mapa(int x, int y) {
         this.x = x;
         this.y = y;
+        this.mapWidth = 1460;
         this.mapaImg = new Obrazok("/Users/filipdavid/Documents/INF/TransBotInteliJ/src/pics/maps/robo.png");
         this.mapaImg.zmenPolohu(x, y);
         this.mapaImg.zobraz();
     }
     
     public void tik() {
-        if (this.x > -1450) { //polovica obrazka - 10  1620 × 242
+        if (this.x > -1620) { //polovica obrazka - 10  1620 × 242
             this.x -= 10;
             this.mapaImg.posunVodorovne(-10);
         } else {
-            this.x = 2430; // sirka obrazka + polovica obrazka
+            this.x = 1610; // sirka obrazka + polovica obrazka
             this.mapaImg.zmenPolohu(this.x, this.y);
         }
     }
