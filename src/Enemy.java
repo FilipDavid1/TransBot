aefbf39a72abda862621cd6c830f8ab82fb1628e
FilipDavid1 <filipenkodavid@gmail.com>
2023-12-28 20:42:08 +0100

    add elblink and luvoque movement to enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,6 +1,8 @@
 import fri.shapesge.DataObrazku;
 import fri.shapesge.Obrazok;
-import fri.shapesge.DataObrazku;
+
+import java.util.Random;
+
 /**
  * Write a description of class Enemy here.
  *
@@ -8,15 +10,18 @@ import fri.shapesge.DataObrazku;
  * @version (a version number or a date)
  */
 public class Enemy {
-    private Obrazok enemyImg;
+    private final Obrazok enemyImg;
     private int x;
     private int y;
-    private int imageWidth;
-    private int imageHeight;
+    private final int imageWidth;
+    private final int imageHeight;
     private boolean movingDown;
-    private boolean movingRight;
 
     private EnemyType type;
+
+    private int randomNumber;
+
+    private int angle;
     
     public Enemy(int x, int y, EnemyType type) {
         this.type = type;
@@ -29,7 +34,9 @@ public class Enemy {
         this.enemyImg.zmenPolohu(this.x, this.y);
         this.enemyImg.zobraz();
         this.movingDown = false;
-        this.movingRight = false;
+        Random random = new Random();
+        this.randomNumber = random.nextInt(800);
+        this.angle = 0;
     }
 
 
@@ -43,13 +50,12 @@ public class Enemy {
             this.moveUp();
         } else if (this.type == EnemyType.BIFLER) {
             this.moveLeft();
-        } else if (this.type == EnemyType.BARRIER) {
-            this.moveUp();
-            this.moveDown();
+        } else if (this.type == EnemyType.ELBLINK) {
+            this.elblinkMovement();
+        } else if (this.type == EnemyType.LUVOGUE) {
+            this.luvoqueMovement();
         }
     }
-
-    //move enemy
 
     private void moveDown() {
         if (this.y < 500 && this.movingDown) {
@@ -80,9 +86,38 @@ public class Enemy {
         }
     }
 
-    public boolean kolizia(Bullet bullet) {
-        return (bullet.getX() >= this.x && bullet.getY() <= this.x + 83 && bullet.getY() >= this.y && bullet.getY() <= this.y + 87);
+    private void elblinkMovement() {
+        //move enemy left until it reaches x that is same as random x then move quickly up or down
+        if (this.x > this.randomNumber) {
+            this.moveLeft();
+        } else {
+            if (this.y < 800 && this.movingDown) {
+                this.enemyImg.posunZvisle(20);
+                this.y += 20;
+
+            } else {
+                this.movingDown = false;
+            }
+            if (this.y >= 0 && !this.movingDown) {
+                this.enemyImg.posunZvisle(-20);
+                this.y -= 20;
+
+            } else {
+                this.movingDown = true;
+            }
+        }
     }
+
+    private void luvoqueMovement() {
+        //spin image constantly and slowly move up and left
+        this.enemyImg.zmenUhol(angle);
+        angle += 10;
+        this.enemyImg.posunZvisle(-2);
+        this.enemyImg.posunVodorovne(-7);
+        this.x -= 7;
+        this.y -= 2;
+    }
+
 
     public int getImageWidth() {
         return imageWidth / 2;
