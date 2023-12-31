1994baba0842e13f4fa6382ce735fb1c6f0a52b9
FilipDavid1 <filipenkodavid@gmail.com>
2023-12-31 18:03:30 +0100

    refactor bullet to differ its direction

--- src/Bullet.java
+++ src/Bullet.java
@@ -14,8 +14,10 @@ public class Bullet {
     private int imageWidth;
     private int imageHeight;
     private BulletType bulletType;
-    
-    public Bullet(int x, int y, BulletType bulletType) {
+
+    private BulletDirection bulletDirection;
+
+    public Bullet(int x, int y, BulletType bulletType, BulletDirection bulletDirection) {
         this.bulletType = bulletType;
         this.nabojImg = new Obrazok("src/pics/amo/" + this.getNazovNaboja() + ".png");
         DataObrazku image = new DataObrazku("src/pics/amo/" + this.getNazovNaboja() + ".png");
@@ -25,17 +27,40 @@ public class Bullet {
         this.y = y;
         this.nabojImg.zmenPolohu(x, y);
         this.nabojImg.zobraz();
+        this.bulletDirection = bulletDirection;
     }
     
     public void tik() {
-        if (this.x <= 1430) {
-            this.x += 30;
-            this.nabojImg.posunVodorovne(30);
-        } else {
-            this.nabojImg.skry();
+        if (this.bulletDirection == BulletDirection.UP) {
+            this.moveUp();
+        } else if (this.bulletDirection == BulletDirection.DOWN) {
+            this.moveDown();
+        } else if (this.bulletDirection == BulletDirection.LEFT) {
+            this.moveLeft();
+        } else if (this.bulletDirection == BulletDirection.RIGHT) {
+            this.moveRight();
         }
     }
 
+    private void moveUp() {
+        this.nabojImg.posunZvisle(-20);
+        this.y -= 20;
+    }
+
+    private void moveDown() {
+        this.nabojImg.posunZvisle(20);
+        this.y += 20;
+    }
+
+    private void moveLeft() {
+        this.nabojImg.posunVodorovne(-20);
+        this.x -= 20;
+    }
+
+    private void moveRight() {
+        this.nabojImg.posunVodorovne(20);
+        this.x += 20;
+    }
     public int getX() {
         return this.x;
     }
@@ -58,9 +83,15 @@ public class Bullet {
                 return "sword_fire";
             case DIFFUSION_BEAM:
                 return "diffusion_beam";
+            case ENEMY_BULLET:
+                return "enemy_bullet";
             default:
                 return "normal";
         }
+    }
+
+    public BulletType getBulletType() {
+        return this.bulletType;
     }
 
     public int getImageWidth() {
@@ -69,5 +100,9 @@ public class Bullet {
 
     public int getImageHeight() {
         return imageHeight / 2;
+    }
+
+    public void zmenUhol(int uhol) {
+        this.nabojImg.zmenUhol(uhol);
     }
 }
---
+++ src/BulletDirection.java
@@ -0,0 +1,10 @@
+public enum BulletDirection {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    UP_RIGHT,
+    DOWN_LEFT,
+    DOWN_RIGHT
+}
